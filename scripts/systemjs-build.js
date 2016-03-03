10900293bbb37a5349deba74e09fe882ec804293
Rogelio Morrell <molekilla@gmail.com>
2016-03-03 16:23:10 -0500

    fixed build

--- scripts/systemjs-build.js
+++ scripts/systemjs-build.js
@@ -1,24 +1,22 @@
 var Builder = require('systemjs-builder'),
-	path = require('path');
+    path = require('path');
 var builder = new Builder();
 
 
 // load SystemJS config from file
 builder
-	.loadConfig('./src/app/system.config.js')
-	.then(function () {
+    .loadConfig('./src/app/system.config.js')
+    .then(function () {
 
 
-		builder.loader.baseURL = path.resolve('./src/');
-		// Change baseURL to match the file system
-		// builder.config({ 
-		// 	baseURL: path.resolve('./src/app/') 
-		// });
-		
+        builder.loader.baseURL = path.resolve('./src/');
+        builder.loader.paths = {
+            "assets/*": "assets/*",
+            "npm:*": "node_modules/*"
+        };
 
-
-		// Build a self-executing bundle (ie. Has SystemJS built in and auto-imports the 'app' module)
-		return builder.buildStatic('app/app.js', 'dist/bundle.js', { minify: true, sourceMaps: true });
-	}).catch(function (err) {
-		console.error(err);
-	});
+        // Build a self-executing bundle (ie. Has SystemJS built in and auto-imports the 'app' module)
+        return builder.buildStatic('app/app.js', 'dist/bundle.js', { minify: true, sourceMaps: true });
+    }).catch(function (err) {
+        console.error(err);
+    });
