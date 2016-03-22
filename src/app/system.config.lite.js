0da6516e12324cba05e8c09ed5919c2331c21255
Rogelio Morrell <molekilla@gmail.com>
2016-03-22 17:30:11 -0500

    updates to systemjs config

--- src/app/system.config.lite.js
+++ src/app/system.config.lite.js
@@ -18,6 +18,7 @@ System.config({
     '_': 'npm:underscore/underscore.js',
     'restangular': 'npm:restangular/dist/restangular',
     'angular-route': 'npm:angular-route/angular-route',    
+    'angular-ui-router': 'npm:angular-ui-router/release/angular-ui-router',
     'angular': 'npm:angular/angular',
     'ng-template': 'npm:plugin-ng-template/ng-template', // buggy
     'text': 'assets/text'
