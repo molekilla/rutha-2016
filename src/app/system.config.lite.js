04f9b75ebbae6e5a08ae5fadd7d8d80e362b2bc9
Rogelio Morrell <molekilla@gmail.com>
2016-06-11 15:38:46 -0500

    moved to ng2

--- src/app/system.config.lite.js
+++ src/app/system.config.lite.js
@@ -1,30 +1,71 @@
 /*globals System:true*/
-// Configure module loader
-System.config({
-  baseURL: '/',
-  defaultJSExtensions: true,
-  meta: {
-    './**/*.html': {
-      loader: 'text'
-    }
-  },
-  paths: {
+
+(function (global) {
+
+  var paths = {
     "./*": "app/*",
     "assets/*": "assets/*",
-    "github:*": "github.com/*",
     "npm:*": "node_modules/*",
-    "@angular/*": "node_modules/@angular/*",
-    "rxjs/*": "node_modules/rxjs/*"    
-  },    
-  map: {
-    'jasmine': 'npm:jasmine-core/lib/jasmine-core/jasmine',
+  };
+
+  // map tells the System loader where to look for things
+  var map = {
     '_': 'npm:underscore/underscore.js',
+    'text': 'assets/text',
+    'rxjs': 'node_modules/rxjs',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    '@angular': 'node_modules/@angular',
+    'jasmine': 'npm:jasmine-core/lib/jasmine-core/jasmine',
     'restangular': 'npm:restangular/dist/restangular',
     'angular-route': 'npm:angular-route/angular-route',    
     'angular-ui-router': 'npm:angular-ui-router/release/angular-ui-router',
     'angular': 'npm:angular/angular',
     'angular-messages': 'npm:angular-messages/angular-messages',
     'ng-template': 'npm:plugin-ng-template/ng-template', // buggy
-    'text': 'assets/text'
+    
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app': { main: 'app.js', defaultExtension: 'js' },
+    'rxjs': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+  };
+
+  var packageNames = [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/router',
+    '@angular/router-deprecated',
+    '@angular/testing',
+    '@angular/upgrade',
+  ];
+
+  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+  packageNames.forEach(function (pkgName) {
+    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
+
+  var config = {
+    meta: {
+      './**/*.html': {
+        loader: 'text'
+      }
+    },
+    baseURL: '/',
+    paths: paths,
+    map: map,
+    packages: packages
   }
-});
+
+  // filterSystemConfig - index.html's chance to modify config before we register it.
+  if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+
+  System.config(config);
+
+})(this);
+
